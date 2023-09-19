ad45fdcede42340415b603fe7d70cf01e8c8134d
BZ85 <zhuxinjie33@gmail.com>
2023-09-19 14:12:11 +0800

    Camera Change

--- Craft_and_Planet/Assets/Scripts/CameraFollower.cs
+++ Craft_and_Planet/Assets/Scripts/CameraFollower.cs
@@ -10,11 +10,14 @@ public class CameraFollower : MonoBehaviour
     {
         
     }
-
+    private Vector3 offset = new Vector3(1, 3, 0);
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if (transform.position.y - craft.transform.position.y > 5) transform.Translate(new Vector3(0f,-10f,0f));
-        if (craft.transform.position.y - transform.position.y > 5) transform.Translate(new Vector3(0f, 10f, 0f));
+        //if (transform.position.y - craft.transform.position.y > 5) transform.Translate(new Vector3(0f,-10f,0f));
+        // if (craft.transform.position.y - transform.position.y > 5) transform.Translate(new Vector3(0f, 10f, 0f));
+
+        if (craft.transform.position.y >= 0 && craft.transform.position.y <= 20) transform.position = craft.transform.position.y* (new Vector3(0f, 1f, 0f)) + new Vector3(0f, 0f, -10f); ;
+       
     }
 }
